bbcd9be71dfaf58f5b3d54b9eeea019ae268a687
jindrahabarta <jindriskuv@email.cz>
2024-08-18 09:35:20 +0200

    edited Hero

--- src/app/_mainPageComponents/Hero.tsx
+++ src/app/_mainPageComponents/Hero.tsx
@@ -12,19 +12,19 @@ const Hero = ({ isNav, navType }: props) => {
         >
             <div className="h-screen w-full sticky top-0 overflow-hidden">
                 <div className="h-full flex flex-col justify-center md:flex-row md:items-center md:gap-4 opacity-0 heroContainer">
-                    <h1 className="sm:text-[22vw] text-[19vw] leading-[22.3vw] origin-left font-rowdies text-darkBlue  heroText">
+                    <h1 className="sm:text-[19vw] text-[19vw] leading-[22.3vw] origin-left font-rowdies text-darkBlue  heroText">
                         Scroll
                     </h1>
                     <div>
                         <p className="font-rowdies lg:leading-[64px] text-[25px]  sm:text-[30px] lg:text-[60px] opacity-0 heroTextSmall">
-                            Forging
+                            Trigger
                         </p>
 
                         <p className="font-rowdies lg:leading-[64px]  text-[25px]  sm:text-[30px] lg:text-[60px] heroTextSmall">
-                            Digital
+                            by
                         </p>
                         <p className="font-rowdies lg:leading-[64px] text-[25px]  sm:text-[30px] lg:text-[60px] heroTextSmall">
-                            Something...
+                            GSAP
                         </p>
                     </div>
                 </div>
